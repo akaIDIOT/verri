0c8afcf2b09d2baddfe4134cea4a70459290f617
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-03-28 23:05:59 +0100

    Add more git things

--- very/git.py
+++ very/git.py
@@ -1,5 +1,6 @@
 import subprocess
-from functools import cache
+
+from very import dates
 
 
 def commit_ts(ref='HEAD'):
@@ -7,10 +8,43 @@ def commit_ts(ref='HEAD'):
     return int(git('show', '--quiet', '--format=%cd', '--date=unix', commit))
 
 
-@cache
+def num_commits_today(ts=None):
+    return num_commits_since(ts=dates.midnight(ts=ts))
+
+
+def num_commits_since(ts):
+    return len(
+        git('log', '--first-parent', f'--since={int(ts.timestamp())}', '--format=%cd', '--date=unix').splitlines(
+            keepends=False
+        )
+    )
+
+
+def clean():
+    return not git('status', '--porcelain', '--untracked-files=no')
+
+
+def branch():
+    return git('branch', '--show-current')
+
+
 def resolve(ref='HEAD'):
     return git('rev-parse', ref)
 
 
 def git(*args):
     return subprocess.check_output(('git', *args), stderr=subprocess.DEVNULL, text=True).strip()
+
+
+def _is_commit(ref):
+    try:
+        return len(bytes.fromhex(ref)) in {20, 32}
+    except TypeError:
+        return False
+
+
+def short(ref='HEAD'):
+    if _is_commit(ref):
+        return ref[:7]
+    else:
+        return short(resolve(ref))

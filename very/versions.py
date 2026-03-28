0abc8f08ebbac7823e6718b3678ca59faf77b250
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-03-28 22:04:00 +0100

    Add very.versions.commit_date

--- very/versions.py
+++ very/versions.py
@@ -1,9 +1,19 @@
 import datetime as dt
 
-from very import version
+from very import git, version
 
 
 @version
 def date(ts=None):
-    now = ts or dt.datetime.now(tz=dt.timezone.utc)
-    return f'{now.year}.{now.month}.{now.day}'
+    match ts:
+        case int() | float():
+            ts = dt.datetime.fromtimestamp(ts, tz=dt.timezone.utc)
+        case None:
+            ts = dt.datetime.now(tz=dt.timezone.utc)
+
+    return f'{ts.year}.{ts.month}.{ts.day}'
+
+
+@version
+def commit_date(ref='HEAD'):
+    return date(ts=git.commit_ts(ref))

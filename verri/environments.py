677a8efc98963821a38f33e3e6a1cd14098835af
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-03-30 15:13:40 +0200

    Distinguish between github and gitlab based on environment variables

--- verri/environments.py
+++ verri/environments.py
@@ -3,3 +3,11 @@ from os import environ
 
 def on_ci():
     return bool(environ.get('CI'))
+
+
+def ci():
+    match environ:
+        case {'GITHUB_ACTIONS': _}:
+            return 'github'
+        case {'GITLAB_CI': _}:
+            return 'gitlab'

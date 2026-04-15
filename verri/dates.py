6c19042e3ff97c9671606afc02d796a36eb142fb
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-04-15 20:17:37 +0200

    Lift UTC now into dates.now()

--- verri/dates.py
+++ verri/dates.py
@@ -1,6 +1,10 @@
 import datetime as dt
 
 
+def now():
+    return dt.datetime.now(tz=dt.timezone.utc)
+
+
 def midnight(ts=None):
-    ts = ts or dt.datetime.now(tz=dt.timezone.utc)
+    ts = ts or now()
     return ts.replace(hour=0, minute=0, second=0, microsecond=0)

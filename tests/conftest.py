ea74d03a85b45275261dc92b79b281f8a8aed98c
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-04-27 20:04:26 +0200

    Add a pytest fixture to extract and cd into a test repository

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import sys
+import tarfile
+from contextlib import contextmanager
 from os import environ
 
 import pytest
@@ -31,3 +34,23 @@ def on_gitlab_cicd(empty_environment, monkeypatch):
     }
     for var, value in env.items():
         monkeypatch.setenv(var, value)
+
+
+@pytest.fixture
+def inside_repo(pytestconfig, monkeypatch, tmp_path_factory):
+    @contextmanager
+    def extract_and_cd(repo_name):
+        repo_dir = tmp_path_factory.mktemp('git-repository')
+        with tarfile.open(pytestconfig.rootpath / 'tests' / 'repos' / repo_name) as repo:
+            # extract the test repository (omitting the filter argument raises warnings in 3.14+, but it's only
+            # available on 3.12+)
+            if sys.version_info < (3, 12):
+                repo.extractall(repo_dir)
+            else:
+                repo.extractall(repo_dir, filter='tar')
+        # cd into the extracted repository for the duration of the test
+        with monkeypatch.context() as mp:
+            mp.chdir(repo_dir)
+            yield
+
+    return extract_and_cd

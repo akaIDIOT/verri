963c4c9ca8f485ed5790e457526495d682d70c2b
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-06-10 17:21:34 +0200

    Add git.shallow_refs() to collect refs that are treated as shallow

--- verri/git.py
+++ verri/git.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 
 from verri import dates, environments
 from verri.errors import CommandNotFound, NoRepository
@@ -41,6 +42,15 @@ def short(ref='HEAD'):
         return ref[:7]
     else:
         return short(resolve(ref))
+
+
+def shallow_refs():
+    git_dir = git('rev-parse', '--git-dir')
+    if (shallow := Path(git_dir) / 'shallow').exists():
+        # .git/shallow exists, listing refs that are treated as shallow
+        return set(shallow.read_text().splitlines(keepends=False))
+    else:
+        return None
 
 
 def git(*args):

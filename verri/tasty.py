b7034cb97b5418c8e598fc6d9cbe6e5183dcf8d5
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-09-15 21:22:29 +0200

    Add docstrings to @version and the available flavours

--- verri/tasty.py
+++ verri/tasty.py
@@ -1,14 +1,36 @@
+"""
+A collection of tasty version flavours.
+"""
+
 from verri import dates, environments, git, version
 
 
 @version
 def mango():
+    """
+    A version flavour that represents the current date (see [`dates.midnight`][verri.dates.midnight]), formatted as
+    `yyyy.mm.dd`, without leading zeroes.
+
+    !!! warning
+
+        This flavour takes nothing but the current date into account, potentially creating the same version number for
+        multiple releases made on the same day.
+    """
     today = dates.midnight()
     return f'{today.year}.{today.month}.{today.day}'
 
 
 @version
 def cherry():
+    """
+    A version flavour much like [mango], formatted as `yyyy.mm.dd.sss`, where `sss` is the number of seconds in the day
+    since [midnight][verri.dates.midnight].
+
+    !!! warning
+
+        Depending on your build system, and the speed at which your project gets built, constructing different versions
+        during the same build process can cause issues. Use with caution.
+    """
     now = dates.now()
     today = dates.midnight(ts=now)
     seconds_today = int((now - today).total_seconds())
@@ -17,6 +39,16 @@ def cherry():
 
 @version(fallback=mango)
 def strawberry():
+    """
+    A version flavour that uses the commit date of `HEAD` and the number of
+    [first parent commits][verri.git.num_commits_since] since midnight on the commit date to construct a version
+    formatted as `yyyy.mm.dd.n`, where `n` is the number of other first parent commits than `HEAD` on the same date.
+
+    !!! fallback
+
+        This flavour will fall back to [`mango`][verri.tasty.mango] flavour on error, see
+        [error handling](caveats.md#error-handling).
+    """
     ref = git.resolve()
     commit_ts = git.commit_ts(ref)
     n = git.num_commits_since(dates.midnight(commit_ts))
@@ -29,6 +61,24 @@ def strawberry():
 
 @version(fallback=mango)
 def pineapple():
+    """
+    A version flavour that uses the commit date of `HEAD` and a number of components to construct a version:
+
+    - the number of [first parent commits][verri.git.num_commits_since] since midnight on the commit date;
+    - whether the version is being determined on a CI/CD environment;
+    - whether the git checkout is clean;
+    - the current git branch is the default branch.
+
+    The resulting version is considered a "release" version if the checks above are all true. A pineapple flavoured
+    release version is much like a [`strawberry`][verri.tasty.strawberry] flavour, although a trailing `.0` will be
+    omitted.
+    If not, a development version number will be created.
+
+    !!! fallback
+
+        This flavour will fall back to [`mango`][verri.tasty.mango] flavour on error, see
+        [error handling](caveats.md#error-handling).
+    """
     ref = git.resolve()
     commit_ts = git.commit_ts(ref)
     commit_version = f'{commit_ts.year}.{commit_ts.month}.{commit_ts.day}'

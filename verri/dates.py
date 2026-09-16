53c8cf6227146dab9eab765196f54adabdf50f18
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-09-16 21:05:40 +0200

    Update docs deps, add docstrings to verri.dates, limit toc depth

--- verri/dates.py
+++ verri/dates.py
@@ -14,16 +14,18 @@ def now() -> dt.datetime:
     """
     The current timezone-aware date and time, in UTC.
 
-    :return:
+    :return: The current date and time in UTC, timezone-aware.
     """
     return dt.datetime.now(tz=dt.timezone.utc)
 
 
-def midnight(ts=None) -> dt.datetime:
+def midnight(ts: dt.datetime | None = None) -> dt.datetime:
     """
+    Midnight of the current or a specified date, in UTC.
 
-    :param ts:
-    :return:
+    :param ts: A specific date to turn into midnight (defaults to the current date). *Note that this needs to be a
+        timezone **aware** datetime, a naive one will be rejected.*
+    :return: A `datetime` at UTC midnight.
     """
     ts = ts or now()
     if not ts.tzinfo:
@@ -32,10 +34,11 @@ def midnight(ts=None) -> dt.datetime:
     return ts.replace(hour=0, minute=0, second=0, microsecond=0)
 
 
-def from_ts(ts) -> dt.datetime:
+def from_ts(ts: int | float) -> dt.datetime:
     """
+    Creates a `datetime` instance of *ts*.
 
     :param ts: A point in time, measured in seconds since the UNIX epoch.
-    :return:
+    :return: A `datetime` instance in UTC.
     """
     return dt.datetime.fromtimestamp(ts, tz=dt.timezone.utc)
